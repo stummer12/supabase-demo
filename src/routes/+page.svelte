d6b2bb85e8bcf166e0c5945e1af484dabbe10acc
stummer12 <paulstummer74@gmail.com>
2024-02-27 14:04:27 +0100

    sdf

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,18 +4,16 @@
 	let promise = supabase.from('countries').select();
 </script>
 
+<div class="prose">
+	<ul>
+		<li>
+			anstatt eines JS_Objekts das auf der Seite dargestellt wir möchte ich eine Liste aller Länder
+		</li>
+		<li>integriere einen Loading Spinner öÄ von daisyUi in dein Projekt</li>
 
-
-<ul>
-    <li>anstatt eines JS_Objekts das auf der Seite dargestellt wir möchte ich eine Liste aller Länder</li>
-    <li>
-        integriere einen Loading Spinner öÄ von daisyUi in dein Projekt
-    </li>
-
-    <li>gib die Vercell-Url zu deinem Projekt in Teams ab.</li>
-</ul>
-
-
+		<li>gib die Vercell-Url zu deinem Projekt in Teams ab.</li>
+	</ul>
+</div>
 
 {#await promise}
 	<div>...loading</div>
