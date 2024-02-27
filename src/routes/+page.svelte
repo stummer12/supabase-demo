3b35f1bf39f4aa0ae4249200054840aea02ddb31
stummer12 <paulstummer74@gmail.com>
2024-02-27 13:48:19 +0100

    adding supabase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
-<div class="prose">also try the options in <code>/src/routes/+layout.js</code>!</div>
+<script>
+	import { supabase } from '$lib/supabase';
+
+	let promise = supabase.from('countries').select();
+</script>
+
+{#await promise}
+	<div>...loading</div>
+{:then result}
+	<div>{JSON.stringify(result)}</div>
+{/await}
